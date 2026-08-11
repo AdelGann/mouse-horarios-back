5bb9ec1adcb6e870711b8ee27cb4a421f346871e
AdelGann <imadelg14@gmail.com>
2026-08-11 17:44:51 -0400

    fix: update CORS setup to support credentials and dynamic origin reflection

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   
   app.enableCors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    origin: true,
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Accept", "Authorization", "X-Requested-With", "Origin", "Bypass-Tunnel-Reminder"],
   })
 
   // Swagger Documentation Setup
